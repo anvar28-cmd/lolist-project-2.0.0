2bf55f59c0d16c77f09b6c3cc4b44bf379188238
Anvar Gulomov <anvar.gulomov.97@gmail.com>
2023-03-04 05:25:45 -0500

    api route add and path add

--- src/components/ui/BuildsItems/BuildsItems.js
+++
@@ -1,7 +0,0 @@
-function BuildsItems() {
-  return (
-    <figure className="builds-item">BuildsItems</div>
-  )
-}
-
-export default BuildsItems
--- src/components/ui/BuildsList/BuildsList.js
+++ src/components/ui/BuildsList/BuildsList.js
@@ -1,22 +1,50 @@
-import { useState } from "react"
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import { AppRoute } from "../../../const";
+import { createAPI } from "../../../services/api";
+import BuildsItem from "../BuildsItem/BuildsItem";
 import Button from "../Button/Button";
 
 function BuildsList() {
+  const params = useParams();
+  const slug = params.slug;
 
-    const [builds, setBuilds] = useState();
+  const [buildss, setBuildss] = useState();
+
+  const handleBuildClick = (evt) => {
+    const api = createAPI();
+    api
+      .get(`${AppRoute.HEROES}/${slug}/builds`)
+      .then(({ data }) => {
+        setBuildss(data);
+        console.log(data);
+      })
+      .catch((error) => console.log(error));
+  }
+
+//   useEffect(() => {
+//     if (slug) {
+//       const api = createAPI();
+//       api
+//         .get(`${AppRoute.HEROES}/${slug}/builds`)
+//         .then(({ data }) => {
+//           setBuilds(data);
+//         })
+//         .catch((error) => console.log(error));
+//     }
+//   }, [slug]);
 
   return (
     <ul className="builds-list">
-        {builds.map((build, key) => (
-            
-            <li key={key} className="builds-list__item">
-                <BuildsItems build={build}/>
-                <Button type="button">Delete</Button>
-                <Button type="delete">edit</Button>
+      <Button onClick={handleBuildClick} type="button">Delete</Button>
+      {/* {builds.map((build, key) => (
+        <li key={key} className="builds-list__item">
+          <BuildsItem build={build} />
+          <Button type="button">Edit</Button>
         </li>
-      ))}
+      ))} */}
     </ul>
-  )
+  );
 }
 
-export default BuildsList
+export default BuildsList;
