0704c7cf83d1659d1ebe4ac959866ca6b80ede76
Anvar Gulomov <anvar.gulomov.97@gmail.com>
2023-03-05 03:02:33 -0500

    Functionality add and css style do

--- src/components/pages/HeroesBuildsPage/HeroesBuildsPage.js
+++ src/components/pages/HeroesBuildsPage/HeroesBuildsPage.js
@@ -1,11 +1,53 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { AppRoute } from "../../../const";
+import { createAPI } from "../../../services/api";
 import Board from "../../ui/Board/Board";
 import BuildsList from "../../ui/BuildsList/BuildsList";
 
 function HeroesBuildsPage() {
+  const params = useParams();
+  const slug = params.slug;
+
+  const [builds, setBuilds] = useState();
+
+  const handleDeleteButton = (evt) => {
+  
+        console.log(builds);
+   
+  };
+
+  const handleEditButton = (evt) => {
+    // const api = createAPI();
+    // api
+    //   .get(`${AppRoute.HEROES}/${slug}/builds`)
+    //   .then(({ data }) => {
+    //     setBuilds(data);
+    //     console.log(data);
+    //   })
+    //   .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    if (slug) {
+      const api = createAPI();
+      api
+        .get(`${AppRoute.HEROES}/${slug}/builds`)
+        .then(({ data }) => {
+          setBuilds(data);
+        })
+        .catch((error) => console.log(error));
+    }
+  }, [slug]);
+
   return (
     <main className="heroes-builds page__main container">
-      <Board title="Hero's builds">
-        <BuildsList />
+      <Board id="build-board" title="Hello">
+        <BuildsList
+          builds={builds}
+          handleDeleteButton={handleDeleteButton}
+          handleEditButton={handleEditButton}
+        />
       </Board>
     </main>
   );
--- src/components/ui/BuildsList/BuildsList.js
+++ src/components/ui/BuildsList/BuildsList.js
@@ -1,48 +1,17 @@
-import { useState } from "react";
-import { useParams } from "react-router-dom";
-import { AppRoute } from "../../../const";
-import { createAPI } from "../../../services/api";
-import BuildsItem from "../BuildsItem/BuildsItem";
+import BuildsItems from "../BuildsItems/BuildsItems";
 import Button from "../Button/Button";
 
-function BuildsList() {
-  const params = useParams();
-  const slug = params.slug;
-
-  const [buildss, setBuildss] = useState();
-
-  const handleBuildClick = (evt) => {
-    const api = createAPI();
-    api
-      .get(`${AppRoute.HEROES}/${slug}/builds`)
-      .then(({ data }) => {
-        setBuildss(data);
-        console.log(data);
-      })
-      .catch((error) => console.log(error));
-  }
-
-//   useEffect(() => {
-//     if (slug) {
-//       const api = createAPI();
-//       api
-//         .get(`${AppRoute.HEROES}/${slug}/builds`)
-//         .then(({ data }) => {
-//           setBuilds(data);
-//         })
-//         .catch((error) => console.log(error));
-//     }
-//   }, [slug]);
+function BuildsList({builds, handleDeleteButton, handleEditButton}) {
 
   return (
     <ul className="builds-list">
-      <Button onClick={handleBuildClick} type="button">Delete</Button>
-      {/* {builds.map((build, key) => (
+      {builds && builds.map((build, key) => (
         <li key={key} className="builds-list__item">
-          <BuildsItem build={build} />
-          <Button type="button">Edit</Button>
+          <BuildsItems build={build} />
+          <Button className="builds-list__button" onClick={handleDeleteButton} type="button">Delete</Button>
+          <Button className="builds-list__button" onClick={handleEditButton} type="button">Edit</Button>
         </li>
-      ))} */}
+      ))}
     </ul>
   );
 }
